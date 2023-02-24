a1c449796df433cb336193824d02591d5eb7fc08
physwizz <pollockg86@gmail.com>
2023-02-24 20:31:34 +1100

    Update readme.md

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # a146p-T
+
+git clone git@github.com:physwizz/a146p-T.git
+
+./build_kernel.sh
--- build_kernel.sh
+++ build_kernel.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-export CROSS_COMPILE=$(pwd)/toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-androidkernel-
-export CC=$(pwd)/toolchain/clang/host/linux-x86/clang-r383902/bin/clang
+make clean && make mrproper
+export CROSS_COMPILE=$(pwd)/toolchain/aarch64-linux-android-4.9-master/bin/aarch64-linux-android-
+export CC=$(pwd)/toolchain/prebuilts_clang_host_linux-x86_clang-r383902-main/bin/clang
 export CLANG_TRIPLE=aarch64-linux-gnu-
 export ARCH=arm64
 
 export KCFLAGS=-w
 export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
 
-make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y a14xm_defconfig
+make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y physwizz_defconfig
 make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j16
 
 cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
